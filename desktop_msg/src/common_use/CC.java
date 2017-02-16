45ba5e24891525738639bf37b846b6283df782ad
dtd-PC\dtd <dbtmddus128@gmail.com>
2017-02-17 05:53:37 +0900

    from desktop

---
+++ desktop_msg/src/common_use/CC.java
@@ -0,0 +1,40 @@
+package common_use;
+
+public class CC {
+	public static String client_command = "client command";
+	
+	public static String id ="id";
+	public static String pw ="pw";
+
+	public static String SignUp_Id ="id";
+	public static String SignUp_Pw ="pw";
+	
+	public static String fid = "f_id";
+	
+	public static String f_id_n = "f_id_n";
+	public static String chatting_msg = "chatting_msg";
+	
+	public static String f_id= "f_id";
+	
+	public static String _id = "_id";
+	
+	
+	public static String b_approved = "b_approved";
+	
+	public static String logged_in_id_n ="logged_in_id_n"; 
+	
+	public static String num_of_friend = "num_of_friend";
+
+	public static String friend_ip = "friend_ip";
+	public static String friend_port = "friend_port";
+
+	public static String str_stop = "str_stop";
+	public static String chat_record = "chat_record";
+	
+//	public static String ListenAndRecord_Str = "listen_and_record : str";
+	
+	//public static String
+	
+	
+	
+}
---
+++ messenger_remake/src/common_use/write.java
@@ -0,0 +1,38 @@
+package common_use;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.nio.charset.Charset;
+import java.util.Vector;
+
+
+public class write {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		for (int i=0; i<10; i++){
+			String str = i +" / "+ Long.toString(System.currentTimeMillis())+"\n";
+			record_txt("client.txt",str);
+			record_txt("server.txt",str);
+		}
+		
+		for (int i=10; i<20; i++){
+			String str = i +" / "+ Long.toString(System.currentTimeMillis())+"\n";
+			record_txt("server.txt",str);
+		}
+	}
+
+	public static void record_txt(String file_name, String msg) throws IOException{
+		File f = new File(".\\qwe\\"+file_name);		
+
+		String ss = f.getParentFile().toString();
+		new File(ss).mkdirs();
+
+		FileOutputStream f_out = new FileOutputStream(f, true);
+		f_out.write(msg.getBytes(Charset.forName("EUC-KR")));	//EUC-KR-윈도우, UTF-8 리눅스.
+		f_out.close();
+	}	
+
+}
